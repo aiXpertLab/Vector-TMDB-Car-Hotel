2ff255305abbaad02d561f88c9ec815be98bcd49
aiXpertLab <aiXpertLab@gmail.com>
2024-04-05 09:30:52 -0400

    local or not

--- pages/1_📊1_Just_ChromaDB.py
+++ pages/1_📊1_Just_ChromaDB.py
@@ -9,8 +9,18 @@ from utils.st_def import st_logo, st_load_book
 st_logo(title='Welcome 👋 to Chroma DB!', page_title="Chroma DB ",)
 st_load_book()
 #-----------------------------------------------
+import os
+
+def is_running_on_streamlit_hosting():
+    return "STREAMLIT_SERVER_ENABLED" in os.environ and "IS_STREAMLIT_SERVER" in os.environ
+
+if is_running_on_streamlit_hosting():
+    print("Running on Streamlit hosting")
+else:
+    print("Running locally")
+
 st.write(logger.get_logger("SMI_APP"))
-st.write(f"{sys.version}")
+st.write(f"sys version: {sys.version}")
 st.header(f"sqlite version: {sqlite3.sqlite_version}")
 #-----------------------------------------------
 chroma_client = chromadb.Client()
