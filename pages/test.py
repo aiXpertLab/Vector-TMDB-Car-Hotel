ede6bfdb2fcd4042ef6cd45c45bef283e1841cb6
aiXpertLab <aiXpertLab@gmail.com>
2024-04-05 00:04:47 -0400

    pysqlite3

---
+++ pages/test.py
@@ -0,0 +1,11 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+import streamlit as st
+from streamlit import logger
+import sqlite3
+
+app_logger = logger.get_logger("SMI_APP")
+app_logger.info(f"{sqlite3.sqlite_version}")
+app_logger.info(f"{sys.version}")
