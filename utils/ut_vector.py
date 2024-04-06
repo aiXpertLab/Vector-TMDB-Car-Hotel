e0a801ccd91cc80a453a5821ef8167138c84d1e8
aiXpertLab <aiXpertLab@gmail.com>
2024-04-06 19:30:50 -0400

    v

--- utils/ut_vector.py
+++ utils/ut_vector.py
@@ -1,8 +1,4 @@
 import sys, os
-
-# __import__('pysqlite3') 
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 import sqlite3
 import chromadb
 
@@ -10,3 +6,16 @@ def chroma_collection(name):
     chroma_client = chromadb.Client()
     collection = chroma_client.get_or_create_collection(name=name)
     return collection
+
+
+
+# import sys, os, platform
+# import streamlit as st
+
+# print(platform.processor()[:7]=="Intel64")
+
+# if not platform.processor()[:7]=="Intel64":
+#   __import__('pysqlite3'); 
+#   sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# else:
+#   st.write('local')
